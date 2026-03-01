842ff89585d045ba49fb2fc36e0ddae76e785fb9
Vlad Che <vchernichenko13@gmail.com>
2026-03-01 19:49:36 +0400

    initial

---
+++ src/main/java/che/weddingbe/WeddingBeApplication.java
@@ -0,0 +1,13 @@
+package che.weddingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeddingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/che/weddingbe/WeddingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package che.weddingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
